4900ad0937eb37823b9d02df1f26a1be1b05326a
miguel santos <miguelcfcs@hotmail.com>
2024-09-28 20:31:42 -0300

    Adicionando Layout e menu hamburguer

---
+++ app/src/main/java/com/example/salvadortech/HomeActivity.kt
@@ -0,0 +1,37 @@
+package com.example.salvadortech
+
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+
+        // Configurando o ícone do menu
+        val menuIcon: ImageView = findViewById(R.id.menu_icon)
+        menuIcon.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                drawerLayout.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
+
